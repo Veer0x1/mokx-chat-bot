390cea9990c26e8f8177cc88ae4039153ed6dc51
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-06-30 13:06:16 +0530

    Empty screen fix

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'What is the mantra in Rigveda 10.2.3?',
+    message: `What is the mantra in Rigveda 10.2.3?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'What are the prescribed Vedic remedies for snake bites?',
+    message: 'What are the prescribed Vedic remedies for snake bites?'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Can you tell me the significance of the Gayatri Mantra?',
+    message: `Can you tell me the significance of the Gayatri Mantra?`
   }
 ]
 
@@ -24,18 +24,11 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to Mokx AI Chatbot!
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
+
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          You can ask queries like:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
