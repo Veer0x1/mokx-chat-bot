524a0ed5db5e656e83e7162d2aed857ec32a15ad
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-03 01:33:01 +0530

    checking session cookies in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
+import {auth} from "@/auth";
+import { redirect } from 'next/navigation'
+
 export { auth as middleware } from './auth'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+
+export function isAuthorized(req: NextRequest) {
+    console.log('hello from isAuthorized middleware')
+    // console.log('loging auth from middleware', auth)
+    const cookies = req.cookies.get('next-auth.session-token')
+    if(cookies === undefined) {
+        return redirect('/register')
+    }
+}
+
+// export const config = {
+//     matcher: '/api/:path*',
+// }
